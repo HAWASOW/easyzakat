b902dab5c9a1319f798c6b653e557ab8b8b76811
HAWASOW <hsow55329@gmail.com>
2026-09-15 11:09:31 +0000

    ajouter tout les fichierst

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Route de test
+app.get("/", (req, res) => {
+  res.json({
+    message: "API EasyZakat fonctionne correctement"
+  });
+});
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur EasyZakat lancé sur http://localhost:${PORT}`);
+});
